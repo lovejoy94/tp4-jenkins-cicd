7ed8e0f84f5ce3f7262ca231762c255b10b7f567
lesley joyce <tchouanfangnzali@gmail.com>
2026-02-09 05:57:08 +0100

    test webhook trigger

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Jenkins CI/CD ");
+  res.send('Hello Jenkins CI/CD  webhook OK');
+
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
